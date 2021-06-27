c9c1405f439fef95daa511defc7184fce7587e87
Tarunsahu2001 <86557918+Tarunsahu2001@users.noreply.github.com>
2021-06-27 08:53:24 -0400

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class main
+{
+	public static void main (String args[])
+	{
+	  Scanner sc=new Scanner (system.in);
+	  System.out.println("enter a num");
+	  int num=sc.nextint();
+	  if ((num^1)==num+1)
+	    {
+		System.out.println("the entered num"+num+'is even");
+	    }
+		else
+		{
+		    system.ot.println("the entered num "+num+"is odd");
+		}
+	}
+}
