ff6fe52168ca6833bac9495425ef655f5a08b2f1
Tarunsahu2001 <86557918+Tarunsahu2001@users.noreply.github.com>
2021-07-01 18:51:41 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,23 @@
+class alphabet
+{
+	public static void main (String[]args)
+		{
+		int alphabet=6;
+		for (int i=1;i<6;i++)
+			{
+			for (int j=1;j<=i;j+=)
+				{
+				System.out.print((char)alphabet);
+				alphabet++;
+				}
+					System.out.println();
+			}
+		}
+}
+
+output:-
+	A
+	BC
+	DEF
+	GHIJ
+	KLMNO
