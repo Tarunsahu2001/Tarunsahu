9541cefb312e3b6ba77354c3fa8f8a6a388e5d15
Tarunsahu2001 <86557918+Tarunsahu2001@users.noreply.github.com>
2021-07-06 19:02:42 +0530

    Add files via upload

---
+++ maxminproduct.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+class maxminproduct
+{
+	public static int minMaxProduct(int arr1[],int arr2[],int n1,int n2)
+		{
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		return arr1[n1-n]*arr2[0];
+		}
+			public static void main(String argc[])
+				{
+				int[]arr1=new int[]{10,2,3,6,4,1};
+				int[]arr2=new int[]z[514269z];
+				int n1=6;
+				int n2=6
+				System.out.println(minMaxProduct(arr1,arr2,n1,n2));
+				}
+}
+
+Output:- -1      
